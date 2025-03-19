9f0179104f157ce7aaf4890334351aaf86627eee
Jack Zammit <jackzammit@hotmail.com>
2025-03-19 18:02:24 -0500

    edits to ApIService.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.squareup.logging.interceptor)
+    implementation(project(":backend"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
