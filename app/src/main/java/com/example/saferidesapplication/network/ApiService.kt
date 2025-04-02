45b03c317b93723d8b46a0dcef10e585f9506fa1
rafa11costa <rafa11.costa@icloud.com>
2025-04-02 12:23:13 -0500

    A couple of layout changes

--- app/src/main/java/com/example/saferidesapplication/network/ApiService.kt
+++ app/src/main/java/com/example/saferidesapplication/network/ApiService.kt
@@ -4,7 +4,7 @@ import com.example.saferidesapplication.network.dto.*
 import retrofit2.Response
 import retrofit2.http.*
 
-const val BASE_URL = "http://138.236.201.218:8080"
+const val BASE_URL = "http://138.236.250.115:8080"
 
 interface ApiService {
 
